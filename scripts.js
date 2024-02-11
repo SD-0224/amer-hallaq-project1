1b3c17af89a44142313774fcbdd3e1087259a351
Amer Hallaq <amerhalaq91@gmail.com>
2024-02-11 16:30:53 +0200

    first commit

---
+++ scripts.js
@@ -0,0 +1,30 @@
+var ratingFields= document.getElementsByClassName("stars")
+
+var arrayOfRating=[4.1,3.5,3.3,4.5,2.5,4.4]
+
+for(let i=0;i<ratingFields.length;i++) {
+
+  ratingFields[i].innerHTML = getStars(arrayOfRating[i]);
+
+}
+
+function getStars(rating) {
+
+  // Round to nearest half
+  rating = Math.round(rating * 2) / 2;
+  let output = [];
+
+  // Append all the filled whole stars
+  for (var i = rating; i >= 1; i--)
+    output.push('<i class="fa fa-star" aria-hidden="true" style="color: gold;"></i>&nbsp;');
+
+  // If there is a half a star, append it
+  if (i == .5) output.push('<i class="fa fa-star-half-o" aria-hidden="true" style="color: gold;"></i>&nbsp;');
+
+  // Fill the empty stars
+  for (let i = (5 - rating); i >= 1; i--)
+    output.push('<i class="fa fa-star-o" aria-hidden="true" style="color: gold;"></i>&nbsp;');
+
+  return output.join('');
+
+}
